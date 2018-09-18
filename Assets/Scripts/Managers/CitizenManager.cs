ca1e54557677927ee567d8bf33e1a76d810b3c54
Alejandro <ale.bros@hotmail.com>
2018-09-18 17:45:40 -0300

    Implementing Citizens rescue logic

---
+++ Assets/Scripts/Managers/CitizenManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitizenManager : MonoBehaviour
+{
+    GameObject[] citizenGroups;
+
+    void Start()
+    {
+        citizenGroups = GameObject.FindGameObjectsWithTag("CitizenGroup");
+
+        GameObject[] enemyGroups = GameObject.FindGameObjectsWithTag("ZombieGroup");
+
+        //citizenGroups.
+    }
+}
---
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] UnityEvent onGroupEliminated;
+
+    GameObject[] enemyGroups;
+
+    void Start()
+    {
+        enemyGroups = GameObject.FindGameObjectsWithTag("ZombieGroup");
+
+        Life[] lifes = FindObjectsOfType<Life>();
+
+        foreach (Life life in lifes)
+        {
+            if (life.gameObject.layer == LayerMask.NameToLayer("Zombies"))
+                life.OnDeath.AddListener(CheckGroupsState);
+        }
+    }
+
+    void CheckGroupsState()
+    {
+        foreach (GameObject enemyGroup in enemyGroups)
+        {
+            if(enemyGroup.transform.childCount == 0)
+            {
+                
+            }
+        }
+    }
+
+    public UnityEvent OnGroupEliminated
+    {
+        get { return onGroupEliminated; }
+    }
+}

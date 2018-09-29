80ca64239c6affd486b7d1a523b37a1ea3d61447
Ignacio Mosconi <nacho7577@hotmail.com>
2018-09-29 16:34:11 -0300

    Improved Mini-map & Buildings

--- Assets/Scripts/Camera/MinimapCameraPosition.cs
+++ Assets/Scripts/Camera/MinimapCameraPosition.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class MinimapCameraPosition : MonoBehaviour
 {
     [SerializeField] Transform player;
+    [SerializeField] float heightOffset;
 
     void LateUpdate()
     {
         Vector3 newPosition = player.position;
-        newPosition.y = transform.position.y;
+        newPosition.y += heightOffset;
         transform.position = newPosition;
 
         transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
